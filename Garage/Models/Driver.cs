072efb4ee71b798a3aa064d6f63caf8c05bb5556
john_gould <jgould@bluerunnersolutions.com>
2018-12-11 17:13:04 +0000

    validations added

--- Garage/Models/Driver.cs
+++ Garage/Models/Driver.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Garage.Models
 {
     public class Driver
     {
         public int DriverID { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public int Age { get; set; }
     }
 }
